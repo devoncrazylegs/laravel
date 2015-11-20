c0bff67ed4845c1e3d9230b8e72dc5076332f69e
James Holman <jamesholman@blurgroup.com>
2015-11-20 15:48:18 +0000

    vagrant box working

--- Vagrantfile
+++ Vagrantfile
@@ -1,7 +1,7 @@
 Vagrant.configure("2") do |config|
 
     # Set Vagrant box to use
-    config.vm.box = "primalskill/ubuntu-trusty64"
+    config.vm.box = "ubuntu/trusty64"
 
     # Configure port forwarding
     config.vm.network :forwarded_port, guest: 80, host: 8931, auto_correct: true
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 echo "Provisioning virtual machine..."
+    apt-get update
 
 echo "Installing Git"
     apt-get install git -y > /dev/null
 
 echo "Installing Nginx"
     apt-get install nginx -y >/dev/null
+
+echo "Configuring Nginx"
+    cp /var/www/nginx_vhost /etc/nginx/sites-available/nginx_vhost > /dev/null
+
+    ln -s /etc/nginx/sites-available/nginx_vhost /etc/nginx/sites-enabled/nginx_vhost
+
+    rm -rf /etc/nginx/sites-available/default
+
+    service nginx restart > /dev/null
 
 echo "Updating PHP repository"
     apt-get install python-software-properties build-essential -y > /dev/null
@@ -18,11 +28,14 @@ echo "Installing PHP"
 
 echo "Installing PHP extensions"
     apt-get install curl php5-curl php5-gd php5-mcrypt php5-mysql -y > /dev/null
+    apt-get install libapache2-mod-php5
 
 echo "Installing MYSQL extensions"
     apt-get install debconf-utils -y > /dev/null
-debconf-set-selections <<< "mysql-server mysql-server/root_password password 1234"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 1234"
+    debconf-set-selections <<< "mysql-server mysql-server/root_password password 1234"
+    debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 1234"
 
-apt-get install mysql-server -y > /dev/null
+    apt-get install mysql-server -y > /dev/null
+
+
 
